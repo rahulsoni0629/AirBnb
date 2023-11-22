fb08a6f451b2f6817622954fee629cad6fd1107c
Rahul Soni <rahulsoni.0629@gmail.com>
2023-11-22 10:11:08 +0530

    login functionality

--- routes/user.js
+++ routes/user.js
@@ -2,17 +2,33 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+
 
 router.get("/signup", (req,res) => {
     res.render("users/signup.ejs");
 })
 
 router.post("/signup", wrapAsync(async(req,res)=>{
-    let{username,email,password} = req.body;
-    const newUser = new User({email,username});
-    await User.register(newUser,password);
-    req.flash("success",`You have successfully registered ! ${username}`);
-    res.redirect("/listings");
+    try{
+        let{username,email,password} = req.body;
+        const newUser = new User({email,username});
+        await User.register(newUser,password);
+        req.flash("success",`You have successfully registered with username ${username}`);
+        res.redirect("/listings");
+    } catch(err) {
+        req.flash("error", err.message);
+        res.redirect("/signup");
+    }
 }));
+
+router.get("/login", (req,res) => {
+    res.render("users/login.ejs")
+});
+
+router.post("/login", passport.authenticate("local",{failureRedirect: "/login", failureFlash:true}), async(req,res) => {
+    req.flash("success","Welcome back to WanderLust");
+    res.redirect("/listings");
+});
 
 module.exports = router;
